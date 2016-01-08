6156697418490e8a27fb829dfd952614c03e51a4
IMOKURI <nenegi.01mo@gmail.com>
2016-01-08 14:53:39 +0900

    Initial commit

---
+++ src/Main.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Network.Wai
+import Network.Wai.Handler.Warp (run)
+import Network.HTTP.Types
+
+main :: IO ()
+main = do
+  putStrLn "http://localhost:8080/"
+  run 8080 app
+
+app :: Application
+app request respond = respond $ case rawPathInfo request of
+  "/"     -> index
+  "/raw/" -> plainIndex
+  _       -> notFound
+
+index :: Response
+index = responseFile
+  status200
+  [("Content-Type", "text/html")]
+  "index.html"
+  Nothing
+
+plainIndex :: Response
+plainIndex = responseFile
+  status200
+  [("Content-Type", "text/plain")]
+  "index.html"
+  Nothing
+
+notFound :: Response
+notFound = responseLBS
+  status404
+  [("Content-Type", "text/plain")]
+  "404 - Not Found"
+
